7c5112be39c11abe2d6ec8f87c735e4916fc3a16
Filip Staniul <22367410+fstaniul@users.noreply.github.com>
2026-03-10 14:27:21 +0100

    fix: set tmp to external directory

--- lua/custom/plugins/99.lua
+++ lua/custom/plugins/99.lua
@@ -26,7 +26,7 @@ return {
       -- and generation will fail refer to tool documentation to resolve
       -- https://opencode.ai/docs/permissions/#external-directories
       -- https://code.claude.com/docs/en/permissions#read-and-edit
-      tmp_dir = './tmp',
+      tmp_dir = '~/.opencode/tmp',
 
       --- Completions: #rules and @files in the prompt buffer
       completion = {
