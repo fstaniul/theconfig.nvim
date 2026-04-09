8183f19b84091d649177736e1bd1ffa3ce68a717
Filip Staniul <22367410+fstaniul@users.noreply.github.com>
2026-04-09 08:19:37 +0200

    feat: allow local overrides and additional options, that don't fit into shared

--- init.lua
+++ init.lua
@@ -1,7 +1,12 @@
 require 'core.options'
-pcall(require, 'core.local_options')
+pcall(require, 'core.options_local')
+
 require 'core.keymap'
+pcall(require, 'core.keymap_local')
+
 require 'core.autocmd'
+pcall(require, 'core.autocmd_local')
+
 require 'core.lazy'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
